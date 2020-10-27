5223f1b3cb36949459a55e799d370a2334a01797
Saveliy <cavacava1999@mail.ru>
2020-10-27 04:34:06 +0300

    solve Task2 (наконец-то)

--- src/main/java/ru/mail/polis/ads/part5_SaveliySchur/Task2_3969.java
+++ src/main/java/ru/mail/polis/ads/part5_SaveliySchur/Task2_3969.java
@@ -4,42 +4,29 @@ import java.util.Scanner;
 
 public class Task2_3969 {
 
-    private static int diploms(double a, double w, double h){
-        return  (int)Math.floor(a/w) * (int)Math.floor(a/h);
+    public static long solv() {
+        Scanner scanner = new Scanner(System.in);
+        long w = scanner.nextLong();
+        long h = scanner.nextLong();
+        long n = scanner.nextLong();
+
+        long a = Math.max(w, h);
+        long b = Math.max(w, h) * n;
+
+
+        while (a < b){
+            long mid = (a+b) / 2;
+            long count = (mid/h) * (mid/w);
+            if(count >= n){
+                b = mid;
+            } else {
+                a = mid+1;
+            }
+        }
+        return a;
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int w = scanner.nextInt();
-        int h = scanner.nextInt();
-        int n = scanner.nextInt();
-
-        double a = Math.max(w, h);
-        double b = Math.max(w, h) * n;
-        double eps = 0.00001;
-        double answer = a;
-
-        while ((b-a) > eps){
-            double mid = (a+b) / 2;
-            if(diploms(mid, w, h) == n){
-                answer = mid;
-                break;
-            }
-            if(diploms(mid, w,h) < n){
-                a = mid;
-            } else {
-                b = mid;
-            }
-            answer = b;
-        }
-        if(diploms(Math.floor(answer)-1, w, h) >= n){
-            answer= Math.floor(answer)-1;
-        } else if(diploms(Math.floor(answer), w, h) >= n){
-            answer= Math.floor(answer);
-        } else{
-            answer = Math.ceil(answer);
-        }
-        System.out.printf("%.0f\n", answer);
-
+        System.out.println(solv());
     }
 }
