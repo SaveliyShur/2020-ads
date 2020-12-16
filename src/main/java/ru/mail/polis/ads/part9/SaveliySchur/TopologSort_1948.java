ad661d904346162d308b0a4c0e934a05c0698361
Saveliy <cavacava1999@mail.ru>
2020-12-16 03:41:58 +0300

    task1 solve gooood

--- src/main/java/ru/mail/polis/ads/part9/SaveliySchur/TopologSort_1948.java
+++ src/main/java/ru/mail/polis/ads/part9/SaveliySchur/TopologSort_1948.java
@@ -3,33 +3,29 @@ package ru.mail.polis.ads.part9.SaveliySchur;
 import ru.mail.polis.ads.SolveTemplate;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class TopologSort_1948 {
-
+    private static class ExceptionForsort extends Exception {
+    }
     private static final Stack<Integer> STACK = new Stack<>();
     //Белый - 0, серый - 1, черный - 2
     private static final byte WHITE = 0;
     private static final byte GREY = 1;
     private static final byte BLACK = 2;
     private static byte[] color;
-    private static List<Integer>[] listSmeghnosty;
 
-
-    private static void dfs(int top){
+    private static void dfs(Graph G, int top) throws ExceptionForsort {
         if(color[top] == BLACK){
             return;
         }
         color[top] = 1;
-        for(Integer child : listSmeghnosty[top]){
+        for(int child : G.adj(top)){
             if(color[child] == GREY){
-                throw new RuntimeException("Найден цикл");
+                throw new ExceptionForsort();
             }
             if(color[child] == WHITE){
-                dfs(child);
+                dfs(G, child);
             }
         }
         color[top] = BLACK;
@@ -37,38 +33,54 @@ public class TopologSort_1948 {
     }
 
     private static void solve(final FastScanner in, final PrintWriter out) {
-        int n = in.nextInt();
-        int m = in.nextInt();
-        listSmeghnosty = new List[n+1];
-        for(int i = 0; i < n + 1; i++){
-            listSmeghnosty[i] = new ArrayList<>(n);
-        }
-        //создаем список смежности, не используем ноль
-        for(int i = 0; i < m; i++){
-            int d = in.nextInt();
-            int p = in.nextInt();
-            listSmeghnosty[d].add(p);
-        }
-        color = new byte[n + 1];
-        for(int i = 1; i < n + 1; i++){
-            if(color[i] == WHITE){
-                try {
-                    dfs(i);
-                } catch (RuntimeException e){
-                    System.out.println(-1);
-                    return;
+        Graph graph = new Graph(in);
+        color = new byte[graph.V];
+        try {
+            for(int i = 0; i < graph.V; i++){
+                if(color[i] == WHITE){
+                    dfs(graph, i);
                 }
             }
+            while (!STACK.empty()){
+                System.out.print(STACK.pop()+1 + " ");
+            }
+        } catch (ExceptionForsort e) {
+            System.out.println("-1");
         }
-        for(int i = 1; i < n +1; i++){
-            if(color[i] != BLACK){
-                out.println(-1);
-                return;
+    }
+
+    static class Graph {
+        private final int V;
+        private int E;
+        private final List<Integer>[] adj;
+        public Graph(int V){
+            this.V = V;
+            this.E = 0;
+            adj = new List[V];
+            for(int v = 0; v < V; v++){
+                adj[v] = new ArrayList<>();
             }
         }
-        System.out.println();
-        while (!STACK.empty()){
-            System.out.print(STACK.pop() + " ");
+
+        public Graph(FastScanner in){
+            this(in.nextInt());
+            int E = in.nextInt();
+            for(int i = 0; i < E; i++){
+                int v = in.nextInt()-1;
+                int w = in.nextInt()-1;
+                addEdge(v,w);
+            }
+
+        }
+
+        public int V(){ return V; }
+        public int E(){ return E; }
+        public void addEdge(int v, int w){
+            adj[v].add(w);
+            E++;
+        }
+        public Iterable<Integer> adj(int v){
+            return adj[v];
         }
     }
 
